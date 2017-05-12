845cecf46d22cbbb9e2c893875e001966eb671c3
Adam Watson <adam@adaw.me>
2017-05-12 11:34:36 +0930

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# rust-oxide-skins-config-generator
+
+Generates config section of all skins for http://oxidemod.org/plugins/skins.2431/ (process_data.txt)
+
+Regex needs some work
+
+1. Put contents of https://raw.githubusercontent.com/OxideMod/Docs/master/source/includes/rust/item_skins_list.md into data.txt
+2. node index.js
+3. processed_data.txt output, maybe JSON format or something first.
---
+++ index.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+var output = {};
+
+var currentItem = '';
+
+fs.readFile('data.txt', 'utf8', function (error, data) {
+  if (error) {
+    return console.log(err);
+  }
+
+  lines = data.split(/\r\n|\r|\n/g);
+
+  for (var i = 0; i < lines.length; i++) {
+    var line = lines[i];
+
+    var match = /\(([^)]+)\)/.exec(line);
+    if (match) {
+      currentItem = match[1];
+      output[currentItem] = [];
+      continue;
+    }
+
+    if (line.startsWith('| ')) {
+      var match = /([0-9]+)(\s+[|])/.exec(line);
+
+      if (match) {
+        var match2 = /([0-9]+)/.exec(match[1])
+
+        if (match2) {
+          output[currentItem].push(parseInt(match2[1]));
+        }
+      }
+    }
+  }
+
+  output = JSON.stringify(output);
+
+  fs.writeFile('processed_data.txt', output, function(error) {
+    if (error) {
+      return console.log(error);
+    }
+  });
+});
